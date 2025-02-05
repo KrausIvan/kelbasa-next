ab85997e7f04a671e6186903c96a36e57c29c046
Ivan Kraus <ivan.kraus@post.cz>
2025-02-05 10:22:00 +0100

    opravy

--- tom-cms/src/app/page.tsx
+++ tom-cms/src/app/page.tsx
@@ -7,51 +7,57 @@ import LoginSharpIcon from '@mui/icons-material/LoginSharp';
 import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import { IconButton } from "@mui/material";
 import styles from "@/app/page.module.css";
+import { Key } from "react";
 
 export default async function HomePage() {
 
-  const articles = await prisma.article.findMany({
-    where: { published: true },
-    take: 20,
-    select: { title: true, author: true, slug: true, createdAt: true, content: true },
-    orderBy: { createdAt: "desc" },
-  });
+    const articles = await prisma.article.findMany({
+        where: {published: true},
+        take: 20,
+        select: {title: true, author: {select: {name: true}}, slug: true, createdAt: true, content: true},
+        orderBy: {createdAt: "desc"},
+    });
 
-  return (
-    <main className={styles.container}>
-      <div className={styles.navigation}>
-        <IconButton
-          color="primary"
-          aria-label="search"
-          href="/search"
-          >
-          <SearchSharpIcon />
-        </IconButton>
-        <IconButton
-          color="primary"
-          aria-label="login"
-          href="/login"
-          >
-          <LoginSharpIcon />
-        </IconButton>
-      </div>
-      <h1 className={styles.heading}>NextJS Times</h1>
-      <p className={styles.description}>
-        The latest articles from the NextJS Times
-      </p>
-      <ul className={styles.articleList}>
-        {articles.map((article) => (
-          <li key={article.slug}>
-            <ArticleCard 
-              title={article.title} 
-              slug={article.slug} 
-              author={article.author.name ?? "Unknown author"} 
-              description={article.content.slice(0, 100)}
-              date={article.createdAt.toLocaleDateString()}
-            />
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+    const serializedArticles = articles.map((article: { createdAt: { toISOString: () => any; }; }) => ({
+        ...article,
+        createdAt: article.createdAt.toISOString(),
+    }));
+
+    return (
+        <main className={styles.container}>
+            <div className={styles.navigation}>
+                <IconButton
+                    color="primary"
+                    aria-label="search"
+                    href="/search"
+                >
+                    <SearchSharpIcon/>
+                </IconButton>
+                <IconButton
+                    color="primary"
+                    aria-label="login"
+                    href="/login"
+                >
+                    <LoginSharpIcon/>
+                </IconButton>
+            </div>
+            <h1 className={styles.heading}>NextJS Times</h1>
+            <p className={styles.description}>
+                The latest articles from the NextJS Times
+            </p>
+            <ul className={styles.articleList}>
+                {serializedArticles.map((article: { slug: Key | null | undefined; title: string; author: { name: any; }; content: string; createdAt: string | number | Date; }) => (
+                    <li key={article.slug}>
+                        <ArticleCard
+                            title={article.title}
+                            slug={article.slug}
+                            author={article.author?.name ?? "Unknown author"}
+                            description={article.content.slice(0, 100)}
+                            date={new Date(article.createdAt).toLocaleDateString()}
+                        />
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
